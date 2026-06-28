9197532cf8c2ab83b9427e8bb70ca941d55359fc
jyr <X20060617611@163.com>
2026-06-28 11:37:54 +0800

    修复图1和图3图片：使用正确中文字体重新生成

--- obj/Debug/net9.0-windows/MediaPlayer.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/MediaPlayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediaPlayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f7757adddf993d431f6b3f34b0eafb696fda4e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13ee9f09f270b7417c732fecbd59ba8974b5a75c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediaPlayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediaPlayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
